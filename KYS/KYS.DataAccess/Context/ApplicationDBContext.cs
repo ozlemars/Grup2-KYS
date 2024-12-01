eb537e36dff1b15afcfc1a0f1552c8576e7edefa
Hüseyin Genç <huseyingenc.eem@gmail.com>
2024-12-01 16:46:06 +0300

    NKatman Mimarisi
    Katmanlı mimari oluşturuldu.

---
+++ KYS/KYS.DataAccess/Context/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace KYS.DataAccess.Context
+{
+    public class ApplicationDBContext:DbContext
+    {
+
+        // buraya aşağıdaki yorum satırı gibi entity eklemeleri yapılacak.
+        // public DbSet<Product> Products { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // app.config'den connection string okuma
+            string connectionString = ConfigurationManager.ConnectionStrings["KYS"].ConnectionString;
+
+            // Bağlantı dizesini DbContext'e ekleme
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+    }
+}
---
+++ KYS/KYS.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace KYS.Entities.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; }=DateTime.Now;
+        public DateTime UpdatedDate { get; set; }
+
+    }
+
+    public class Duyurular : BaseEntity
+    {
+        public string Baslik { get; set; } // Duyuru başlığı
+        public string Icerik { get; set; } // Duyuru içeriği
+        public DateTime YayınTarihi { get; set; } // Yayınlanma tarihi
+    }
+}
